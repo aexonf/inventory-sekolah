0c3fc6e1b510dffd37800cae17ff580487d50457
aexonf <echo.fajarf@gmail.com>
2024-05-19 16:51:13 +0700

    IG: crud item

---
+++ app/Http/Controllers/Back/ItemsController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Back;
+
+use App\Http\Controllers\Controller;
+use App\Models\Items;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class ItemsController extends Controller
+{
+
+    public function create(Request $request) {
+        $request->validate([
+            "item_number" => "required",
+            "name" => "required",
+            "stock" => "required",
+            "image" => "image|mimes:jpeg,png,jpg,gif,svg",
+            "description" => "required",
+        ]);
+
+        $image = null;
+
+        if ($request->hasFile('image')) {
+            $rand = Str::random(8);
+            $file_name = $rand . "-" . $request->file('image')->getClientOriginalName();
+            $request->file('image')->move('storage/upload/items/', $file_name);
+            $image = $file_name;
+        }
+
+        $item = Items::create([
+            "item_number" => $request->item_number,
+            "name" => $request->name,
+            "stock" => $request->stock,
+            "description" => $request->description,
+            "category_id" => $request->category,
+            "image" => $image
+        ]);
+
+        if ($item) {
+            return redirect()->back()->with("success", "Data saved successfully");
+        }
+
+        return redirect()->back()->with("error", "Something went wrong");
+    }
+
+
+    public function edit($id, Request $request) {
+        $findItem = Items::find($id);
+
+        if(!$findItem) {
+            return redirect()->back()->with("error", "Item not found");
+        }
+
+        $image = null;
+
+        if ($request->hasFile('image')) {
+            $rand = Str::random(8);
+            $file_name = $rand . "-" . $request->file('image')->getClientOriginalName();
+            $request->file('image')->move('storage/upload/items/', $file_name);
+            $image = $file_name;
+        }
+
+        $updateItem = $findItem->update([
+            "item_number" => $request->item_number,
+            "name" => $request->name,
+            "stock" => $request->stock,
+            "description" => $request->description,
+            "category_id" => $request->category,
+            "image" => $image
+        ]);
+
+        if ($updateItem) {
+            return redirect()->back()->with("success", "Item updated successfully");
+        }
+
+        return redirect()->back()->with("error", "Something went wrong");
+
+    }
+
+    public function delete($id) {
+        $findItem = Items::find($id);
+
+        $delete = $findItem->delete();
+
+        if($delete) {
+            return redirect()->back()->with("success", "Item deleted successfully");
+        }
+
+        return redirect()->back()-with("error", "Something went wrong");
+    }
+
+
+
+}
---
+++ app/Imports/StudentImport.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Student;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class StudentImport implements ToModel
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new Student([
+            //
+        ]);
+    }
+}
