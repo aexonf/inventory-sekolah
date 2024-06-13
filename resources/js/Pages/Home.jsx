f775647cbf671800c152e3c0b3872baf0b2c5189
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-06-13 15:17:23 +0700

    feat: scan qr & preview qr data

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,12 +1,83 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { Button, Card } from "../components/ui";
 import { User, ScanLine } from "lucide-react";
 import { Link } from "@inertiajs/inertia-react";
+import QrReader from "react-qr-scanner";
+import QrCodeIcon from "../../../public/img/qr-code.svg";
 
 const Home = () => {
+    const [result, setResult] = useState("");
+    const [isScannerOpen, setIsScannerOpen] = useState(false);
+
+    const handleScan = useCallback((data) => {
+        if (data) {
+            setResult(data);
+        }
+    }, []);
+
+    const handleError = useCallback((err) => {
+        console.error(err);
+    }, []);
+
+    const previewStyle = {
+        height: "340px",
+        width: "266px",
+        objectFit: "fill",
+        borderRadius: "10px",
+    };
+
+    console.log(result);
+
+    const toggleScanner = () => {
+        setIsScannerOpen((prevState) => !prevState);
+        setResult("");
+    };
+
     return (
-        <div className="h-screen flex justify-center">
-            <div className="w-[400px] py-[20px]">
+        <div className="h-screen flex justify-center items-center">
+            <div
+                className={`${
+                    isScannerOpen
+                        ? "w-[346px] h-[466px] justify-between"
+                        : "w-[240px] h-[331px]"
+                } flex flex-col items-center  rounded-[10px] py-[20px] px-[39px] bg-[#F7F4FF]`}
+            >
+                {isScannerOpen && result === "" ? (
+                    <QrReader
+                        delay={100}
+                        style={previewStyle}
+                        onError={handleError}
+                        onScan={handleScan}
+                    />
+                ) : result !== "" ? (
+                    <div>{result.text}</div>
+                ) : (
+                    <>
+                        <img src={QrCodeIcon} alt="" />
+
+                        <h2 className="font-bold text-[18px] mt-[20px]">
+                            Scan QR Code
+                        </h2>
+                        <p className="text-[12px] mt-[9px] text-center font-bold text-[#414141]">
+                            Scan QR Code to get student information
+                        </p>
+                    </>
+                )}
+
+                <button
+                    className="mt-[23px] w-full bg-[#7B4DF6] bg-opacity-[45%] text-white font-semibold rounded-[10px] py-[10px] text-[14px]"
+                    onClick={toggleScanner}
+                >
+                    {isScannerOpen && result === ""
+                        ? "Cancel Camera"
+                        : result !== ""
+                        ? "Clear"
+                        : "Open Camera"}
+                </button>
+                {isScannerOpen && <div></div>}
+            </div>
+            {/* <>{result}</> */}
+            {/* <div className="w-[400px] py-[20px]">
                 <div className="flex items-center gap-3">
                     <div className="flex justify-center items-center h-[45px] w-[45px] border-[1px] border-solid border-neutral-800 rounded-full">
                         <User />
@@ -32,7 +103,7 @@ const Home = () => {
                         </div>
                     </Link>
                 </Card>
-            </div>
+            </div> */}
         </div>
     );
 };
