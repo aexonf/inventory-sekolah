83c8298e94bd0ed5f3d884f7629ea11595e9f668
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-06-19 11:40:11 +0700

    feat: add close & information icon

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -7,9 +7,11 @@ import QrCodeIcon from "../../../public/img/qr-code.svg";
 const Home = () => {
     const [result, setResult] = useState("");
     const [isScannerOpen, setIsScannerOpen] = useState(false);
+    const [alertOpen, setAlertOpen] = useState(false);
 
     const handleScan = useCallback((data) => {
         if (data) {
+            setAlertOpen(true);
             setResult(data);
         }
     }, []);
@@ -25,8 +27,6 @@ const Home = () => {
         borderRadius: "10px",
     };
 
-    console.log(result);
-
     const toggleScanner = () => {
         setIsScannerOpen((prevState) => !prevState);
         setResult("");
@@ -34,7 +34,7 @@ const Home = () => {
 
     return (
         <>
-            <main className="h-auto w-full max-w-[420px] mx-auto pb-[110px]">
+            <main className="h-auto w-full max-w-[420px] mx-auto pb-[110px] relative">
                 <div className="px-[25px] flex items-center bg-primary-low gap-2 py-[5px]">
                     <CircleUserRound className="h-[42px] w-[42px] stroke-1" />
                     <h1 className="text-[14px] flex flex-col justify-center leading-[17px]">
@@ -51,33 +51,48 @@ const Home = () => {
                     } mt-[50px] mx-auto transition-all duration-150 flex flex-col items-center  rounded-[10px] py-[20px] bg-[#F7F4FF]`}
                 >
                     {isScannerOpen && result === "" ? (
-                        <div className="w-full">
-                            <QrReader
-                                delay={100}
-                                style={previewStyle}
-                                onError={handleError}
-                                onScan={handleScan}
-                            />
-                            <Button
-                                className="mt-[23px]"
-                                onClick={toggleScanner}
-                            >
-                                Cancel Scan
-                            </Button>
-                        </div>
-                    ) : result !== "" ? (
-                        <div className="h-full w-full flex flex-col justify-between">
-                            <p>{`${result.text}`}</p>
-
-                            <Button
-                                className="mt-[23px]"
-                                onClick={toggleScanner}
-                            >
-                                Clear Data
-                            </Button>
+                        <>
+                            <div className="w-full">
+                                <QrReader
+                                    delay={100}
+                                    style={previewStyle}
+                                    onError={handleError}
+                                    onScan={handleScan}
+                                />
+                                <Button
+                                    className="mt-[23px]"
+                                    onClick={toggleScanner}
+                                >
+                                    Cancel Scan
+                                </Button>
+                            </div>
+                        </>
+                    ) : result !== "" && alertOpen ? (
+                        <div className="bg-red-500 absolute top-[20px] z-20">
+                            <h1>Hello</h1>
                         </div>
                     ) : (
+                        // <div className="h-full w-full flex flex-col justify-between">
+                        //     <p>{`${result.text}`}</p>
+
+                        //     <Button
+                        //         className="mt-[23px]"
+                        //         onClick={toggleScanner}
+                        //     >
+                        //         Clear Data
+                        //     </Button>
+                        // </div>
                         <>
+                            <div className="w-full bg-[#D1C6ED] absolute top-[20px] z-20">
+                                <div>
+                                    <div>
+                                        <img src="" alt="" />
+                                        <h1></h1>
+                                    </div>
+                                    <div>x</div>
+                                </div>
+                                <p></p>
+                            </div>
                             <h2 className="font-bold text-[20px]">
                                 Scan QR Code
                             </h2>
