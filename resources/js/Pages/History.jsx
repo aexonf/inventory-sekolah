f97867510c9ef9b885ff15bc673a02745954dc7f
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-06-23 21:18:49 +0700

    feat: verify user in register page

--- resources/js/Pages/History.jsx
+++ resources/js/Pages/History.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Navigation } from "../components/ui/navigation";
+import axios from "axios";
+import { Inertia } from "@inertiajs/inertia";
+import Cookies from "js-cookie";
 
 const dummyData = [
     {
@@ -60,34 +63,76 @@ const dummyData = [
 ];
 
 export default function History() {
+    const [isVerifyUser, setIsVerifyUser] = useState(true);
+    const inventoryToken = Cookies.get("inventory_token");
+
+    const verifyUser = async () => {
+        setIsVerifyUser(true);
+        try {
+            const getUser = await axios("/api/user", {
+                headers: {
+                    Authorization: `Bearer ${inventoryToken}`,
+                },
+            });
+
+            setIsVerifyUser(false);
+        } catch (error) {
+            console.log(error);
+            if (error.response.data.message === "Unauthenticated.") {
+                Inertia.visit("/login");
+                setIsVerifyUser(false);
+                return;
+            }
+        }
+    };
+
+    useEffect(() => {
+        verifyUser();
+    }, []);
+
     return (
         <>
-            <main className="h-screen w-full max-w-[420px] mx-auto pb-[110px]">
-                <div className="px-[25px] flex items-center bg-primary-low gap-1 py-[20px]">
-                    <h1 className="flex flex-col justify-center font-semibold leading-[17px] text-[20px]">
-                        History
-                    </h1>
-                </div>
-                <div className="mt-[30px] flex flex-col gap-4 px-[20px] pb-[100px]">
-                    {dummyData.map((item, index) => (
-                        <div
-                            className="bg-[#E1D6FF] w-full bg-opacity-[25%] rounded-md px-[25px] py-[15px]"
-                            key={index}
-                        >
-                            <div className="flex justify-between items-center">
-                                <h1 className="font-medium text-[17px]">
-                                    {item.title}
-                                </h1>
-                                <p className="text-[13px]">{item.date}</p>
-                            </div>
-                            <p className="text-[13px] mt-[4px]">
-                                Jumlah Pinjaman: {item.total}
-                            </p>
+            {!isVerifyUser && (
+                <>
+                    {" "}
+                    <main className="h-screen w-full max-w-[420px] mx-auto pb-[110px]">
+                        <div className="px-[25px] flex items-center bg-primary-low gap-1 py-[20px] justify-between">
+                            <h1 className="flex flex-col justify-center font-semibold leading-[17px] text-[20px]">
+                                History
+                            </h1>
+                            <button
+                                onClick={() => {
+                                    Cookies.remove("inventory_token");
+                                    Inertia.visit("/");
+                                }}
+                            >
+                                Logout
+                            </button>
                         </div>
-                    ))}
-                </div>
-            </main>
-            <Navigation />
+                        <div className="mt-[30px] flex flex-col gap-4 px-[20px] pb-[100px]">
+                            {dummyData.map((item, index) => (
+                                <div
+                                    className="bg-[#E1D6FF] w-full bg-opacity-[25%] rounded-md px-[25px] py-[15px]"
+                                    key={index}
+                                >
+                                    <div className="flex justify-between items-center">
+                                        <h1 className="font-medium text-[17px]">
+                                            {item.title}
+                                        </h1>
+                                        <p className="text-[13px]">
+                                            {item.date}
+                                        </p>
+                                    </div>
+                                    <p className="text-[13px] mt-[4px]">
+                                        Jumlah Pinjaman: {item.total}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                    </main>
+                    <Navigation />
+                </>
+            )}
         </>
     );
 }
--- resources/js/Pages/Register.jsx
+++ resources/js/Pages/Register.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Input, Button } from "../components/ui/index";
 import { Link } from "@inertiajs/inertia-react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { Inertia } from "@inertiajs/inertia";
+import Cookies from "js-cookie";
 
 export default function Register() {
     const {
@@ -13,6 +14,8 @@ export default function Register() {
         formState: { errors },
     } = useForm();
     const [isLoading, setIsLoading] = useState(false);
+    const [verifyLoading, setIsVerifyLoading] = useState(true);
+    const inventoryToken = Cookies.get("inventory_token");
 
     const onSubmit = async (data) => {
         const { username, email, password, id_number, address, phone_number } =
@@ -48,180 +51,218 @@ export default function Register() {
         }
     };
 
+    const verifyUser = async () => {
+        setIsVerifyLoading(true);
+
+        try {
+            const { data: getUser } = await axios("/api/user", {
+                headers: {
+                    Authorization: `Bearer ${inventoryToken}`,
+                },
+            });
+
+            console.log(getUser);
+            if (getUser.username !== "") {
+                Inertia.visit("/");
+                setIsVerifyLoading(false);
+            }
+        } catch (error) {
+            console.log(error);
+            if (error.response.data.message === "Unauthenticated.") {
+                Inertia.visit("/login");
+                setIsVerifyLoading(false);
+                return;
+            }
+        }
+    };
+
+    useEffect(() => {
+        if (inventoryToken === undefined || inventoryToken === "") {
+            setIsVerifyLoading(false);
+            return;
+        } else {
+            verifyUser();
+        }
+    }, []);
+
     return (
-        <div className="pt-[40px] pb-[50px] gap-[60px] flex justify-center flex-col items-center w-full max-w-[420px] mx-auto">
-            <div>
-                <h1 className="text-center leading-8 text-[27px]">
-                    Create Account
-                </h1>
-                <p className="text-center">Create a New Account</p>
-            </div>
-            <form
-                className="flex flex-col gap-8 rounded-md px-[30px] w-full"
-                onSubmit={handleSubmit(onSubmit)}
-            >
-                <label htmlFor="username" className="flex flex-col">
-                    <span className=" text-neutral-800 leading-3 mb-[6px]">
-                        Username
-                    </span>
-                    <Input
-                        {...register("username", {
-                            required: "Username is Required",
-                        })}
-                        value={watch("username")}
-                        id="username"
-                        className={`${
-                            errors.username &&
-                            "outline-red-500 focus:outline-red-400"
-                        }`}
-                        type="text"
-                        placeholder="Username. . ."
-                    />
-                    {errors.username && (
-                        <span className="text-[13px] mt-[4px] text-red-500">
-                            {errors.username.message}
-                        </span>
-                    )}
-                </label>
-
-                <label htmlFor="password" className="flex flex-col">
-                    <span className="text-neutral-800 leading-3 mb-[6px]">
-                        Password
-                    </span>
-                    <Input
-                        {...register("password", {
-                            required: "Password is Required",
-                        })}
-                        value={watch("password")}
-                        id="password"
-                        className={`${
-                            errors.password &&
-                            "outline-red-500 focus:outline-red-400"
-                        }`}
-                        type="text"
-                        placeholder="Password. . ."
-                    />
-                    {errors.password && (
-                        <span className="text-[13px] mt-[4px] text-red-500">
-                            {errors.password.message}
-                        </span>
-                    )}
-                </label>
-
-                <label htmlFor="email" className="flex flex-col">
-                    <span className="text-neutral-800 leading-3 mb-[6px]">
-                        Email
-                    </span>
-                    <Input
-                        {...register("email", {
-                            required: "Email is Required",
-                        })}
-                        value={watch("email")}
-                        id="email"
-                        type="text"
-                        placeholder="Email. . ."
-                        className={`${
-                            errors.email &&
-                            "outline-red-500 focus:outline-red-400"
-                        }`}
-                    />
-                    {errors.email && (
-                        <span className="text-[13px] mt-[4px] text-red-500">
-                            {errors.email.message}
-                        </span>
-                    )}
-                </label>
-
-                <label htmlFor="nis" className="flex flex-col">
-                    <span className="text-neutral-800 leading-3 mb-[6px]">
-                        Nis
-                    </span>
-                    <Input
-                        {...register("id_number", {
-                            required: "Nis is Required",
-                        })}
-                        value={watch("id_number")}
-                        id="nis"
-                        type="text"
-                        placeholder="Nis. . ."
-                        className={`${
-                            errors.id_number &&
-                            "outline-red-500 focus:outline-red-400"
-                        }`}
-                    />
-                    {errors.id_number && (
-                        <span className="text-[13px] mt-[4px] text-red-500">
-                            {errors.id_number.message}
-                        </span>
-                    )}
-                </label>
-
-                <label htmlFor="no" className="flex flex-col">
-                    <span className="text-neutral-800 leading-3 mb-[6px]">
-                        No HP
-                    </span>
-                    <Input
-                        {...register("phone_number", {
-                            required: "Phone Number is Required",
-                        })}
-                        value={watch("phone_number")}
-                        id="no"
-                        type="text"
-                        placeholder="No. . ."
-                        className={`${
-                            errors.phone_number &&
-                            "outline-red-500 focus:outline-red-400"
-                        }`}
-                    />
-                    {errors.phone_number && (
-                        <span className="text-[13px] mt-[4px] text-red-500">
-                            {errors.phone_number.message}
-                        </span>
-                    )}
-                </label>
-
-                <label htmlFor="address" className="flex flex-col">
-                    <span className="text-neutral-800 leading-3 mb-[6px]">
-                        Address
-                    </span>
-                    <Input
-                        {...register("address", {
-                            required: "Address is Required",
-                        })}
-                        value={watch("address")}
-                        id="address"
-                        type="text"
-                        placeholder="Address. . ."
-                        className={`${
-                            errors.address &&
-                            "outline-red-500 focus:outline-red-400"
-                        }`}
-                    />
-                    {errors.address && (
-                        <span className="text-[13px] mt-[4px] text-red-500">
-                            {errors.address.message}
-                        </span>
-                    )}
-                </label>
-
-                <div>
-                    <Button
-                        className="bg-[#A27FFE] mt-[50px]"
-                        disable={isLoading}
+        <>
+            {!verifyLoading && (
+                <div className="pt-[40px] pb-[50px] gap-[60px] flex justify-center flex-col items-center w-full max-w-[420px] mx-auto">
+                    <div>
+                        <h1 className="text-center leading-8 text-[27px]">
+                            Create Account
+                        </h1>
+                        <p className="text-center">Create a New Account</p>
+                    </div>
+                    <form
+                        className="flex flex-col gap-8 rounded-md px-[30px] w-full"
+                        onSubmit={handleSubmit(onSubmit)}
                     >
-                        <span className="text-lg">Register</span>
-                    </Button>
-                    <p className="text-center text-[13px] mt-[6px]">
-                        Already have an account ?{" "}
-                        <Link
-                            href="/login"
-                            className="text-[#A27FFE] font-semibold hover:underline"
-                        >
-                            Login
-                        </Link>
-                    </p>
+                        <label htmlFor="username" className="flex flex-col">
+                            <span className=" text-neutral-800 leading-3 mb-[6px]">
+                                Username
+                            </span>
+                            <Input
+                                {...register("username", {
+                                    required: "Username is Required",
+                                })}
+                                value={watch("username")}
+                                id="username"
+                                className={`${
+                                    errors.username &&
+                                    "outline-red-500 focus:outline-red-400"
+                                }`}
+                                type="text"
+                                placeholder="Username. . ."
+                            />
+                            {errors.username && (
+                                <span className="text-[13px] mt-[4px] text-red-500">
+                                    {errors.username.message}
+                                </span>
+                            )}
+                        </label>
+
+                        <label htmlFor="password" className="flex flex-col">
+                            <span className="text-neutral-800 leading-3 mb-[6px]">
+                                Password
+                            </span>
+                            <Input
+                                {...register("password", {
+                                    required: "Password is Required",
+                                })}
+                                value={watch("password")}
+                                id="password"
+                                className={`${
+                                    errors.password &&
+                                    "outline-red-500 focus:outline-red-400"
+                                }`}
+                                type="text"
+                                placeholder="Password. . ."
+                            />
+                            {errors.password && (
+                                <span className="text-[13px] mt-[4px] text-red-500">
+                                    {errors.password.message}
+                                </span>
+                            )}
+                        </label>
+
+                        <label htmlFor="email" className="flex flex-col">
+                            <span className="text-neutral-800 leading-3 mb-[6px]">
+                                Email
+                            </span>
+                            <Input
+                                {...register("email", {
+                                    required: "Email is Required",
+                                })}
+                                value={watch("email")}
+                                id="email"
+                                type="text"
+                                placeholder="Email. . ."
+                                className={`${
+                                    errors.email &&
+                                    "outline-red-500 focus:outline-red-400"
+                                }`}
+                            />
+                            {errors.email && (
+                                <span className="text-[13px] mt-[4px] text-red-500">
+                                    {errors.email.message}
+                                </span>
+                            )}
+                        </label>
+
+                        <label htmlFor="nis" className="flex flex-col">
+                            <span className="text-neutral-800 leading-3 mb-[6px]">
+                                Nis
+                            </span>
+                            <Input
+                                {...register("id_number", {
+                                    required: "Nis is Required",
+                                })}
+                                value={watch("id_number")}
+                                id="nis"
+                                type="text"
+                                placeholder="Nis. . ."
+                                className={`${
+                                    errors.id_number &&
+                                    "outline-red-500 focus:outline-red-400"
+                                }`}
+                            />
+                            {errors.id_number && (
+                                <span className="text-[13px] mt-[4px] text-red-500">
+                                    {errors.id_number.message}
+                                </span>
+                            )}
+                        </label>
+
+                        <label htmlFor="no" className="flex flex-col">
+                            <span className="text-neutral-800 leading-3 mb-[6px]">
+                                No HP
+                            </span>
+                            <Input
+                                {...register("phone_number", {
+                                    required: "Phone Number is Required",
+                                })}
+                                value={watch("phone_number")}
+                                id="no"
+                                type="text"
+                                placeholder="No. . ."
+                                className={`${
+                                    errors.phone_number &&
+                                    "outline-red-500 focus:outline-red-400"
+                                }`}
+                            />
+                            {errors.phone_number && (
+                                <span className="text-[13px] mt-[4px] text-red-500">
+                                    {errors.phone_number.message}
+                                </span>
+                            )}
+                        </label>
+
+                        <label htmlFor="address" className="flex flex-col">
+                            <span className="text-neutral-800 leading-3 mb-[6px]">
+                                Address
+                            </span>
+                            <Input
+                                {...register("address", {
+                                    required: "Address is Required",
+                                })}
+                                value={watch("address")}
+                                id="address"
+                                type="text"
+                                placeholder="Address. . ."
+                                className={`${
+                                    errors.address &&
+                                    "outline-red-500 focus:outline-red-400"
+                                }`}
+                            />
+                            {errors.address && (
+                                <span className="text-[13px] mt-[4px] text-red-500">
+                                    {errors.address.message}
+                                </span>
+                            )}
+                        </label>
+
+                        <div>
+                            <Button
+                                className="bg-[#A27FFE] mt-[50px]"
+                                disable={isLoading}
+                            >
+                                <span className="text-lg">Register</span>
+                            </Button>
+                            <p className="text-center text-[13px] mt-[6px]">
+                                Already have an account ?{" "}
+                                <Link
+                                    href="/login"
+                                    className="text-[#A27FFE] font-semibold hover:underline"
+                                >
+                                    Login
+                                </Link>
+                            </p>
+                        </div>
+                    </form>
                 </div>
-            </form>
-        </div>
+            )}
+        </>
     );
 }
