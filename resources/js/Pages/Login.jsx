e26493e5d600ba66c7c2029c8c11d3ad89b4a748
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-06-23 20:36:54 +0700

    feat: verify user in login page

--- resources/js/Pages/Login.jsx
+++ resources/js/Pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Input, Button } from "../components/ui/index";
 import { Link } from "@inertiajs/inertia-react";
 import { useForm } from "react-hook-form";
@@ -14,6 +14,8 @@ export default function Login() {
         formState: { errors },
     } = useForm();
     const [isLoading, setIsLoading] = useState(false);
+    const [verifyLoading, setIsVerifyLoading] = useState(true);
+    const inventoryToken = Cookies.get("inventory_token");
 
     const onSubmit = async (data) => {
         const { username, password } = data;
@@ -41,79 +43,119 @@ export default function Login() {
         }
     };
 
+    const verifyUser = async () => {
+        setIsVerifyLoading(true);
+
+        try {
+            const { data: getUser } = await axios("/api/user", {
+                headers: {
+                    Authorization: `Bearer ${inventoryToken}`,
+                },
+            });
+
+            console.log(getUser);
+            if (getUser.username !== "") {
+                Inertia.visit("/");
+                setIsVerifyLoading(false);
+            }
+        } catch (error) {
+            console.log(error);
+            if (error.response.data.message === "Unauthenticated.") {
+                Inertia.visit("/login");
+                setIsVerifyLoading(false);
+                return;
+            }
+        }
+    };
+
+    useEffect(() => {
+        if (inventoryToken === undefined || inventoryToken === "") {
+            setIsVerifyLoading(false);
+            return;
+        } else {
+            verifyUser();
+        }
+    }, []);
+
     return (
-        <div className="pt-[60px] pb-[50px] gap-[60px] flex justify-center flex-col items-center w-full max-w-[420px] mx-auto">
-            <div>
-                <h1 className="text-center leading-8 text-[27px]">
-                    Welcome Back
-                </h1>
-                <p className="text-center">Enter your Credentiol for Login </p>
-            </div>
-            <form
-                className="flex flex-col gap-8 rounded-md px-[30px] w-full"
-                onSubmit={handleSubmit(onSubmit)}
-            >
-                <label className="flex flex-col">
-                    <span className=" text-neutral-800 leading-3 mb-[6px]">
-                        Username
-                    </span>
-                    <Input
-                        {...register("username", {
-                            required: "Username is Required",
-                        })}
-                        value={watch("username")}
-                        type="text"
-                        placeholder="Username. . ."
-                        className={`${
-                            errors.username &&
-                            "outline-red-500 focus:outline-red-400"
-                        }`}
-                    />
-                    {errors.username && (
-                        <span className="text-[13px] mt-[4px] text-red-500">
-                            {errors.username.message}
-                        </span>
-                    )}
-                </label>
+        <>
+            {!verifyLoading && (
+                <div className="pt-[60px] pb-[50px] gap-[60px] flex justify-center flex-col items-center w-full max-w-[420px] mx-auto">
+                    <div>
+                        <h1 className="text-center leading-8 text-[27px]">
+                            Welcome Back
+                        </h1>
+                        <p className="text-center">
+                            Enter your Credentiol for Login{" "}
+                        </p>
+                    </div>
+                    <form
+                        className="flex flex-col gap-8 rounded-md px-[30px] w-full"
+                        onSubmit={handleSubmit(onSubmit)}
+                    >
+                        <label className="flex flex-col">
+                            <span className=" text-neutral-800 leading-3 mb-[6px]">
+                                Username
+                            </span>
+                            <Input
+                                {...register("username", {
+                                    required: "Username is Required",
+                                })}
+                                value={watch("username")}
+                                type="text"
+                                placeholder="Username. . ."
+                                className={`${
+                                    errors.username &&
+                                    "outline-red-500 focus:outline-red-400"
+                                }`}
+                            />
+                            {errors.username && (
+                                <span className="text-[13px] mt-[4px] text-red-500">
+                                    {errors.username.message}
+                                </span>
+                            )}
+                        </label>
 
-                <label className="flex flex-col">
-                    <span className=" text-neutral-800 leading-3 mb-[6px]">
-                        Password
-                    </span>
-                    <Input
-                        {...register("password", {
-                            required: "Password is Required",
-                        })}
-                        value={watch("password")}
-                        type="text"
-                        placeholder="Password. . ."
-                        className={`${
-                            errors.password &&
-                            "outline-red-500 focus:outline-red-400"
-                        }`}
-                    />
-                    {errors.password && (
-                        <span className="text-[13px] mt-[4px] text-red-500">
-                            {errors.password.message}
-                        </span>
-                    )}
-                </label>
+                        <label className="flex flex-col">
+                            <span className=" text-neutral-800 leading-3 mb-[6px]">
+                                Password
+                            </span>
+                            <Input
+                                {...register("password", {
+                                    required: "Password is Required",
+                                })}
+                                value={watch("password")}
+                                type="text"
+                                placeholder="Password. . ."
+                                className={`${
+                                    errors.password &&
+                                    "outline-red-500 focus:outline-red-400"
+                                }`}
+                            />
+                            {errors.password && (
+                                <span className="text-[13px] mt-[4px] text-red-500">
+                                    {errors.password.message}
+                                </span>
+                            )}
+                        </label>
 
-                <div>
-                    <Button className="bg-[#A27FFE] mt-[50px]">
-                        <span className="text-lg">Login</span>
-                    </Button>
-                    <p className="text-center text-[13px] mt-[6px]">
-                        Don’t have an account ?{" "}
-                        <Link
-                            href="/register"
-                            className="text-[#A27FFE] font-semibold hover:underline"
-                        >
-                            Register
-                        </Link>
-                    </p>
+                        <div>
+                            <Button className="bg-[#A27FFE] mt-[50px]">
+                                <span className="text-lg">Login</span>
+                            </Button>
+                            <p className="text-center text-[13px] mt-[6px]">
+                                Don’t have an account ?{" "}
+                                <Link
+                                    href="/register"
+                                    className="text-[#A27FFE] font-semibold hover:underline"
+                                >
+                                    Register
+                                </Link>
+                            </p>
+                        </div>
+                    </form>
                 </div>
-            </form>
-        </div>
+            )}
+        </>
     );
 }
