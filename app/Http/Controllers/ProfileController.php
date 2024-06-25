09d106af0531cf62029452020e4f31b9c043b2de
aexon <aexon@localhost.localdomain>
2024-06-26 00:26:58 +0700

    feat: profile api

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ProfileResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class ProfileController extends Controller
+{
+
+    public function profile(){
+
+        $user = User::with('student.activeStudents')->where("id", auth()->user()->id)->first();
+        $lastActiveStudent = $user->student->activeStudents()->latest()->first();
+        // Check if the user has a student relationship and if the student is active
+        if (!$user->student || !$user->student->activeStudents()->exists()) {
+            abort(403, "User is not a student or is not active");
+        }
+
+        $data = [
+            "id" => $lastActiveStudent->id,
+            "name" => $user->student->name,
+            "id_number" => $user->student->id_number,
+            "address" => $user->student->address,
+            "phone_number" => $user->student->phone_number,
+            "email" => $user->email,
+            "status" => $user->status,
+            "class" => $lastActiveStudent->class,
+            "generation" => $lastActiveStudent->generation,
+            "school_year" => $lastActiveStudent->school_year
+        ];
+
+        // Convert the array to an object to prevent the "Attempt to read property \"id\" on array" error
+        $data = (object)$data;
+
+        return response()->json([
+            "status" => "success",
+            "message" => "Profile fetched successfully",
+            "data" => new ProfileResource($data)
+        ]);
+    }
+
+}
---
+++ app/Http/Resources/ProfileResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProfileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "id_number" => $this->id_number,
+            "address" => $this->address,
+            "phone_number" => $this->phone_number,
+            "email" => $this->email,
+            "status" => $this->status,
+            "class" => $this->class,
+            "generation" => $this->generation,
+            "school_year" => $this->school_year,
+        ];
+    }
+}
