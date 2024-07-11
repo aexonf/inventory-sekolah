4b4d40b7df4adb695ee37661c7332a3fcb4564b0
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-07-11 23:17:44 +0700

    fix: comment unused code

--- resources/js/Pages/Dashboard/Item.jsx
+++ resources/js/Pages/Dashboard/Item.jsx
@@ -11,6 +11,10 @@ function Setting() {
                 <div className="absolute top-[-40px] w-full px-[20px]">
                     <div className="bg-white shadow-[3px_3px_20px_-2px_#00000024]  py-[20px] rounded-md px-[20px]">
                         <h1 className="text-[20px]">Item</h1>
+                        {/* <img
+                            src="/storage/upload/teacher/HxMnlkub-favicon-32x32.png"
+                            alt=""
+                        /> */}
                     </div>
                 </div>
                 <div className="pt-[70px] px-[20px]">
