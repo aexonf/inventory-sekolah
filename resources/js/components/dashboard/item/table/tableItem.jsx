7574e0e11cc211eccd9b715a22e25ed153da0f75
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-07-11 09:57:14 +0700

    feat: list items api

--- resources/js/components/dashboard/item/table/tableItem.jsx
+++ resources/js/components/dashboard/item/table/tableItem.jsx
@@ -1,36 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { columns } from "./columns";
 import { DataTable } from "./dataTable";
+import axios from "axios";
+import { Inertia } from "@inertiajs/inertia";
+import Cookies from "js-cookie";
 
 export default function TableItem() {
-    const data = [
-        {
-            id: 1,
-            id_number: "12345678910",
-            name: "Laptop Acer",
-            description: "hahaah",
-            stock: 1,
-            status: "available",
-            created_at: "2024-06-23T07:44:39.000000Z",
-            updated_at: "2024-06-23T07:44:39.000000Z",
-            image: "https://robohash.org/rizki",
-        },
-        {
-            id: 2,
-            id_number: "12345678910",
-            name: "Laptop Macbook",
-            description: "hahaah",
-            stock: 1,
-            status: "available",
-            created_at: "2024-06-23T07:44:39.000000Z",
-            updated_at: "2024-06-23T07:44:39.000000Z",
-            image: "https://robohash.org/rizki",
-        },
-    ];
+    const inventoryToken = Cookies.get("inventory_token");
+    const [itemsList, setItemsList] = useState([]);
+
+    const getAllItems = async () => {
+        try {
+            const { data: getItems } = await axios("/api/v1/items", {
+                headers: {
+                    Authorization: `Bearer ${inventoryToken}`,
+                },
+            });
+            setItemsList(getItems?.data);
+        } catch (error) {
+            console.log(error);
+            if (error.response.data.message === "Unauthenticated.") {
+                Inertia.visit("/login");
+                return;
+            }
+        }
+    };
+
+    useEffect(() => {
+        getAllItems();
+    }, []);
 
     return (
         <div className="mx-auto max-w-[900px] py-10">
-            <DataTable columns={columns} data={data} />
+            <DataTable columns={columns} data={itemsList} />
         </div>
     );
 }
